35275aacb7ad475516937298afaf2b730cd166d5
ezalorpro <kleiver615@gmail.com>
2020-03-21 00:12:57 -0400

    modificaciones clases

--- plots/forms.py
+++ plots/forms.py
@@ -1,10 +1,7 @@
 from django import forms
 
 class DataForm(forms.Form):
-    x_points = forms.CharField(label='Puntos de X', max_length=200, widget=forms.TextInput(
-        attrs={'class': "input-field col s12"}))
-    
-    y_points = forms.CharField(label='Puntos de Y', max_length=200, widget=forms.TextInput(
-        attrs={'class': "input-field col s12"}))
+    x_points = forms.CharField(label='Puntos de X', max_length=200)
+    y_points = forms.CharField(label='Puntos de Y', max_length=200)
 
     # layout = Layout(x_points, y_points)
