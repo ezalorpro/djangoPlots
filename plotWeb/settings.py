e0087ca50c9e612b32bcdbbe89e9b9ef756ee68e
ezalorpro <kleiver615@gmail.com>
2020-03-30 17:50:06 -0400

    primeros cambios para produccion

--- plotWeb/settings.py
+++ plotWeb/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a87q8knuv#dfe6l5yv&zzqsj75urc#4woun6irxp0-wkhny_7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -125,4 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), '/var/www/static/']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
